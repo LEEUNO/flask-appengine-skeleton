9a53bf27008fbbb10787e1894f8dd59b330a302e
백재현 <ruru0729@yonsei.ac.kr>
2015-09-22 23:17:18 +0900

    README.md update

--- app/main/index.py
+++ app/main/index.py
@@ -12,9 +12,11 @@ def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
 
+
 @app.app_errorhandler(404)
 def page_not_found(e):
     return 'Page not found', 404
+
 
 @app.app_errorhandler(500)
 def internal_server_error(e):
