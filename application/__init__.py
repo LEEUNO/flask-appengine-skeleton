fd23d4d4edcceb93fa2b65cefd0f2808d5c0ac66
jaehyun <ruru0729@yonsei.ac.kr>
2015-11-10 19:44:54 +0900

    done,  add cors

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.cors import CORS
 from application.config import config
 
-
+# it would be used as application module variable
 db = SQLAlchemy()
 
 def create_app(config_name='default'):
+
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     db.init_app(app)
 
     from main import main as main_blueprint
