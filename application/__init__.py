61b602123b64acb204b8a4146d0c9f2a47969bf3
jaehyun <ruru0729@yonsei.ac.kr>
2015-11-25 19:40:35 +0900

    model blueprint delete

--- application/__init__.py
+++ application/__init__.py
@@ -6,8 +6,8 @@ from application.config import config
 # it would be used as application module variable
 db = SQLAlchemy()
 
-def create_app(config_name='default'):
 
+def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
@@ -17,9 +17,7 @@ def create_app(config_name='default'):
     from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from models import models as models_blueprint
-    app.register_blueprint(models_blueprint)
-
     return app
+
 
 app = create_app()
--- application/config.py
+++ application/config.py
@@ -1,4 +1,5 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -6,26 +7,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'df32tgi32(=vfT2G!'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
+    SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms:///main?instance=flask-rest-api:jh?charset=utf8'
+
     @staticmethod
     def init_app(app):
         pass
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms:///main?instance=flask-rest-api:jh?charset=utf8'
-
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms:///main?instance=flask-rest-api:jh?charset=utf8'
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms:///main?instance=flask-rest-api:jh?charset=utf8'
-
-
-config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
-}
-
